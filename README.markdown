b2393665b293a9b5a2525e82b5eecfa06b9d8029
tomrogers123 <tom@therogersfamilyonline.com>
2011-10-23 13:06:56 +0100

    New parameters to provide user options added.

--- README.markdown
+++ README.markdown
@@ -3,19 +3,40 @@
 A simple application, dependant on JavaScript and the JQuery library, that segments HTML forms in order that the user is not overwhelmed with data. It looks for "fieldset" elements (that are the semantic way to define areas of a form) inside of the element that it is invoked on and only displays one at a time.
 
 ### Features
+
+#### Added on 23/10/11
+
+- Added ability to enable progress bar.
+- Added button to go back to previous fieldset (currently dysfunctional )
+- Enabled changing of text on navigational buttons.
+- Added parameter to change animation between sections.
+
+#### Added on 22/10/11
+
 - Simple to implement.
 - No JS dependant markup is targeted.
 - Attaches action to valid elements.
 
 ### Usage
 
-As yet, there are no option you can pass to the function meaning, providing that the plugin file is loaded after JQuery itself, that you just call:
+Providing that the plugin file is loaded after JQuery itself, that you just call:
 
 ```javascript
 
-  $('#some-form').stageForm();
+  $('#some-form').stageForm({
+    
+    // DEFAULT VALUES THAT ARE ASSUMED WITHOUT INCLUSION
+    progress : true,
+    progressColor : "#ccc" // CAN BE ANY FORMAT ACCEPTED BY CSS
+    NextText : "Next",
+    previousText : "Previous",
+    animation : "fade" // CAN ALSO BE SLIDE 
+    
+  });
 
 ```
+
+This relies on the fact that each step of the form is within its own "fieldset" (see demo for working example).
 
 ### Feature Enhancements
 This is my first ever plugin. As I learn more and more, I will actively improve the feature set of this as well as adding various customisation options (such as changing the animation between fieldsets). If you have any suggestions, problems or would like to help make it better, please post to the issue queue. In particular, I would really appreciate it if somebody helped me to make it more efficient.
--- jQuery.stageform.js
+++ jQuery.stageform.js
@@ -1,32 +1,111 @@
 var i = -1;
 
 (function($) {
+    
+  $.fn.stageForm = function(options) {
   
-  $.fn.stageForm = function() {
-
+    var settings = {progress : false,
+                    progressColor : '#ccc',
+                    nextText : 'Next',
+                    previousText : 'Previous',
+                    animation : 'fade'};
+    
+    function changeFieldset(sectionNumber,currContext) {
+      switch (settings.animation) {
+        
+        case 'fade':
+         $(currContext).parent().fadeOut('fast', function() {
+           $(this).siblings('fieldset').eq(sectionNumber).show('fast');
+         });
+        break;
+       
+        case 'slide':
+          $(currContext).parent().slideUp('fast', function() {
+            $(this).siblings('fieldset').eq(sectionNumber).slideDown('fast');
+          });
+        break;
+        
+/* CURRENTLY BROKEN
+        case 'swingOff':
+          $(currContext).parent().animate({
+          
+            right : '99999px'
+          
+          }.show(), function() {
+            $(this).siblings('fieldset').eq(sectionNumber).animate({    
+              left: '0px'
+            });
+      
+          });
+        break;
+        
+*/
+      };
+    
+    };
+    
     return this.each(function() {
-
-      var target = $(this).find('fieldset');
-      target.not(':last').append('<button class="next-section" type="button">Next</button>');
+    
+      if (options) { $.extend( settings, options );};
+      
+      var target = $(this).find('fieldset'),
+          submit =  $(this).find('[type="submit"]'),
+          totalNumSections = 0;
+      
+      target.not(':first').append('<button class="previous-section" type="button">' + settings.previousText + '</button>')
+      target.not(':last').append('<button class="next-section" type="button">' + settings.nextText + '</button>');
       target.last().append('<button class="show-all" type="button">Display All Sections</button')
              .click(function() {
                target.fadeIn();
-               $('.show-all').remove();  
-               $('.next-section').remove()             
+               submit.fadeIn();
+               $('.show-all,.next-section,.previous-section').remove();              
              });
              
       target.not(':first').hide();
+      submit.hide();
+      target.each(function() {  totalNumSections++  });
+      
+      if(settings.progress) {  
+                  
+        $(this).before('<div style="height: 25px; border: 1px solid #ccc; background: #fff"><div class="progress" style="height: 100%; background:' + settings.progressColor + ';">&nbsp;</div></div>');
+      };
             
-      $(this).find('.next-section').click(function() {
-
+      $(this).find('.next-section').live('click', function() {
         i++;
-
-        $(this).parent().fadeOut('fast', function() {
-          $(this).siblings('fieldset').eq(i).show('fast');
-        });
+        changeFieldset(i,this);
+                
+        if(i == totalNumSections - 1) {
+          submit.show();
+        };
+                
+        if(settings.progress) { 
+          
+          var full = 100,
+              click_counter = 0,
+              pre = $('.progress').width(),
+              single = full / totalNumSections,
+              completeWidth = 0;
+              
+          click_counter++;
+          completeWidth = single * click_counter; 
+          completedWidth = completedWidth + 33;
+          
+          $('.progress').animate({
+            width: "" + completeWidth + "%",
+          })
+          
+        };
+        
+      });
+      
+      
+      $(this).find('.previous-section').click(function() {
+        
+        i--;          
+        changeFieldset(i,this);
 
       });
-
+      
     });
 
   };
